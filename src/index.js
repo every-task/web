ab54152db147a76d6caf5b5f762b52de8e8f7176
gotoAbraxas <lty7085@gmail.com>
2023-10-26 11:43:35 +0900

    feat: 테마 적용 #1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
+import { MyCustomTheme } from './component/template/Palette';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <ThemeProvider theme={MyCustomTheme}>
+    <App />
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
