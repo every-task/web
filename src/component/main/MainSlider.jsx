e8b8adc5b73432b7cba30ea5e4786d5a099d9e0e
Amy <wlgpek90@naver.com>
2023-10-30 10:41:55 +0900

    feat: 슬라이더 코드 작성 #9

--- src/component/main/MainSlider.jsx
+++ src/component/main/MainSlider.jsx
@@ -1,7 +1,7 @@
-import { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import MainFeaturedPost from "./MainFeaturedPost";
 
 const MainSlider = () => {
 
@@ -16,18 +16,23 @@ const MainSlider = () => {
         autoplay: true,
         autoplaySpeed: 2500,
         pauseOnFocus: true
-
     }
 
+    const mainFeaturedPost = {
+        title:
+            'Title of a longer featured blog post',
+        description:
+            "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents." +
+            "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+        image: 'https://cdn.pixabay.com/photo/2016/02/11/14/59/background-1193727_1280.png',
+        imageText: 'image',
+        // linkText: 'Continue reading…',
+    };
 
     return (
         <div>
-            <h2> 이 속에서 mui 활용 하는 방법 ? </h2>
             <Slider {...settings}>
-                <div>
-                    <img src='https://cdn.newspenguin.com/news/photo/202006/1920_5382_140.jpg'></img>
-                    글씨가 어떻게 들어가는거지
-                </div>
+                <MainFeaturedPost post={mainFeaturedPost} />
                 <div>
                     <img src='https://cdn.newspenguin.com/news/photo/202006/1920_5382_140.jpg'></img>
                 </div>
@@ -40,9 +45,8 @@ const MainSlider = () => {
                 <div>
                     <img src='https://cdn.newspenguin.com/news/photo/202006/1920_5382_140.jpg'></img>
                 </div>
-                <div>
-                    <h3>6</h3>
-                </div>
+
+                <MainFeaturedPost post={mainFeaturedPost} />
             </Slider>
         </div>
     );
