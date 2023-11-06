1911021eb6dd64ed18f9a11728cc3684d191fb56
gotoAbraxas <lty7085@gmail.com>
2023-11-06 16:22:28 +0900

    refactor: 라우트 수정 #17

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -3,7 +3,7 @@ import Template from "../component/template/Template"
 import Main from "../pages/Main"
 
 import Signup from '../pages/Signup'
-import Login from "../pages/Login"
+import { Login } from "../pages/Login"
 import Post from "../pages/Post"
 
 const MyRoutes = () => {
