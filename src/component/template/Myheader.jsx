47e2a0098c972179d45ab526a298e47e3eedaf66
gotoAbraxas <lty7085@gmail.com>
2023-11-08 15:16:28 +0900

    feat: 지속적 로그인을 위한 로직 추가 #20

--- src/component/template/Myheader.jsx
+++ src/component/template/Myheader.jsx
@@ -1,8 +1,52 @@
-import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Button, MenuItem, Menu, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
+import { apiNoToken } from "../../network/api";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { setLogin, setMe } from "../../feature/meSlice";
+import InfoMenu from "../Myheader/InfoMenu";
+import PostMenu from "../Myheader/PostMenu";
 
 
-const Myheader = () => {
+export const Myheader = () => {
+
+    const dispatch = useDispatch()
+
+    const isLogin = useSelector((state) => state.me.isLogin)
+
+    const welcome = async () => {
+
+
+        const { data } = await apiNoToken('api/v1/auth/token/welcome', 'POST')
+
+        if (data.token) {
+
+            dispatch(setLogin(true))
+            onLoginSuccess(data, isLogin)
+            getData()
+
+        }
+
+
+
+    }
+
+    const getData = async () => {
+        const { data } = await apiNoToken('api/v1/auth/member/me/info', 'GET')
+
+        dispatch(setMe(data))
+
+    }
+
+    useState(() => {
+
+        if (!isLogin) {
+            welcome()
+        } else {
+            getData()
+        }
+    }, [])
 
 
     return (
@@ -16,17 +60,41 @@ const Myheader = () => {
                 >
                     <Link to='/' color='primary'> .Task</Link>
                 </Typography>
+                {isLogin ?
+                    <>
+                        <PostMenu />
+                        <InfoMenu />
+                    </>
+                    :
+                    <>
+                        <Link to="/login"><Button color="text" size="large">Login</Button></Link>
+                        <Link to='/signup'><Button color="primary" size="large">signup</Button></Link>
+                    </>}
 
-                <Link to="/login"><Button color="text" size="large">Login</Button></Link>
-                <Link to='/signup'><Button color="primary" size="large">signup</Button></Link>
             </Toolbar>
         </AppBar>
-
-
-
-
     );
+}
+
+export const onLoginSuccess = (data) => {
+
+    const expireTime = 1000 * 60 * 10 // 10분
+
+    const accessToken = data.token
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+
+
+    const id = setTimeout(onSilentRefresh, expireTime - 60000)
 
 
 }
-export default Myheader
+
+export const onSilentRefresh = async () => {
+
+    const { data } = await apiNoToken('api/v1/auth/token/refresh', 'POST')
+    if (data.token) {
+        onLoginSuccess(data)
+    }
+
+}
