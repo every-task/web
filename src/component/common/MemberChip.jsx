e2e28d56fb0ac6d6eb9e3cfe2f51055301085001
ktk8916 <ktk8916@naver.com>
2023-11-08 17:21:41 +0900

    feat: 멤버 chip 추가

---
+++ src/component/common/MemberChip.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Avatar, Chip } from "@mui/material";
+
+const MemberChip = ({ member }) => {
+  return (
+    <Chip
+      avatar={<Avatar>{member.imageUrl}</Avatar>}
+      label={member.nickname}
+      variant="outlined"
+    />
+  );
+};
+
+export default MemberChip;
