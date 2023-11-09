a2cfe09a823786785f5442af17270c8c57ee3ed8
gotoAbraxas <lty7085@gmail.com>
2023-11-09 09:53:17 +0900

    refactor: 프리티어 설정#16

--- src/component/Myheader/PostMenu.jsx
+++ src/component/Myheader/PostMenu.jsx
@@ -4,49 +4,48 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 const PostMenu = () => {
+  const nav = useNavigate();
 
-    const nav = useNavigate()
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const toSuccessHandler = () => {
+    setAnchorEl(null);
+    nav("/post");
+  };
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const toSuccessHandler = () => {
-        setAnchorEl(null);
-        nav('/post')
-    };
-    const toQuestionHandler = () => {
-        setAnchorEl(null);
-        nav('/question')
-    };
+  const toQuestionHandler = () => {
+    setAnchorEl(null);
+    nav("/question");
+  };
 
-    return <>
-        <Button
-            id="write-button"
-            aria-controls={open ? 'write-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-        >
-            글 작성
-        </Button>
-        <Menu
-            id="write-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={() => setAnchorEl(null)}
-            MenuListProps={{
-                'aria-labelledby': 'write-button',
-            }}
-        >
-            <MenuItem onClick={toSuccessHandler}> 성공담 </MenuItem>
-            <MenuItem onClick={toQuestionHandler}>질문글 </MenuItem>
-        </Menu>
-
+  return (
+    <>
+      <Button
+        id="write-button"
+        aria-controls={open ? "write-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        글 작성
+      </Button>
+      <Menu
+        id="write-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => setAnchorEl(null)}
+        MenuListProps={{
+          "aria-labelledby": "write-button",
+        }}
+      >
+        <MenuItem onClick={toSuccessHandler}> 성공담 </MenuItem>
+        <MenuItem onClick={toQuestionHandler}>질문글 </MenuItem>
+      </Menu>
     </>
+  );
+};
 
-}
-
-
-export default PostMenu
+export default PostMenu;
