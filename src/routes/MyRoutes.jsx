98829e9f9b0310089e60d1a4bd595901acb42fe3
ktk8916 <ktk8916@naver.com>
2023-11-10 09:43:03 +0900

    refactor: url 명확하게 변경

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Template from "../component/template/Template";
 import Main from "../pages/Main";
 import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import Post from "../pages/Post";
+import { Login } from "../pages/Login";
+import StoryPost from "../pages/StoryPost";
 import Story from "../pages/Story";
+import MyInfo from "../pages/MyInfo";
 
 const MyRoutes = () => {
   return (
@@ -14,7 +15,7 @@ const MyRoutes = () => {
         <Route path="/signup" element={<Signup></Signup>} />
         <Route element={<Template />}>
           <Route path="/" element={<Main />} />
-          <Route path="/post" element={<Post />} />
+          <Route path="/story/post" element={<StoryPost />} />
           <Route path="/info" element={<MyInfo />} />
           <Route path="/story/:id" element={<Story />} />
         </Route>
