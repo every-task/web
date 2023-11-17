42e5d5c1da82055bbb752690b6f1337b63db75d7
gotoAbraxas <lty7085@gmail.com>
2023-11-17 16:41:09 +0900

    feat: 카테고리 컴포넌트로 추출 #60

---
+++ src/component/common/Category.jsx
@@ -0,0 +1,37 @@
+export const categorys = [
+  {
+    value: "HEALTH",
+    label: "건강",
+    src: "https://cdn-icons-png.flaticon.com/128/1683/1683155.png",
+  },
+  {
+    value: "TRAVEL",
+    label: "여행",
+    src: "https://cdn-icons-png.flaticon.com/128/6350/6350271.png",
+  },
+  {
+    value: "ART",
+    label: "예술",
+    src: "https://cdn-icons-png.flaticon.com/128/4647/4647153.png",
+  },
+  {
+    value: "RELATIONSHIP",
+    label: "인간관계",
+    src: "https://cdn-icons-png.flaticon.com/128/2640/2640788.png",
+  },
+  {
+    value: "EMPLOYMENT",
+    label: "취업",
+    src: "https://cdn-icons-png.flaticon.com/128/3302/3302647.png",
+  },
+  {
+    value: "STRESS",
+    label: "정신건강",
+    src: "https://cdn-icons-png.flaticon.com/128/2017/2017334.png",
+  },
+  {
+    value: "LANGUAGE",
+    label: "언어",
+    src: "https://cdn-icons-png.flaticon.com/128/3898/3898082.png",
+  },
+];
