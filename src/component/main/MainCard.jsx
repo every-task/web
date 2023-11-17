21ba04e8bf5e05e5de3008fbc84b2c0a8e072164
gotoAbraxas <lty7085@gmail.com>
2023-11-17 14:13:10 +0900

    feat: 카테고리 추가#60

--- src/component/main/MainCard.jsx
+++ src/component/main/MainCard.jsx
@@ -5,6 +5,7 @@ import {
   CardContent,
   CardHeader,
   CardMedia,
+  Chip,
   Container,
   Grid,
   Typography,
@@ -12,12 +13,14 @@ import {
 import { red } from "@mui/material/colors";
 import { apiNoToken } from "../../network/api";
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
+import { Link } from "react-router-dom";
 
 const MainCard = () => {
   const [data, setData] = useState([]);
   const getData = async () => {
     const { data } = await apiNoToken(`/api/v1/story`, "GET");
     setData(data.content);
+    console.log(data.content);
   };
   useEffect(() => {
     getData();
@@ -29,35 +32,44 @@ const MainCard = () => {
         {data &&
           data.map((el, index) => (
             <Grid item key={index} md={4}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardHeader
-                  avatar={
-                    <Avatar
-                      sx={{ bgcolor: red[500] }}
-                      aria-label="user"
-                      src={el.member?.profileImageUrl}
-                    ></Avatar>
-                  }
-                  title={el.member?.nickname}
-                />
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image="https://cdn.pixabay.com/photo/2023/09/21/18/17/automobile-8267369_1280.jpg"
-                  alt="Paella dish"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {el.title}
-                  </Typography>
-                </CardContent>
-              </Card>
+              <Link to={`/story/${el.id}`}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardHeader
+                    avatar={
+                      <Avatar
+                        sx={{ bgcolor: red[500] }}
+                        aria-label="user"
+                        src={el.member?.profileImageUrl}
+                      ></Avatar>
+                    }
+                    title={el.member?.nickname}
+                    action={
+                      <Chip
+                        label={el?.category}
+                        color="primary"
+                        variant="outlined"
+                      />
+                    }
+                  />
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image="https://cdn.pixabay.com/photo/2023/09/21/18/17/automobile-8267369_1280.jpg"
+                    alt="Paella dish"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {el.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Link>
             </Grid>
           ))}
       </Grid>
