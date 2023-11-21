b9ce72c2bd4781c200382859ed039272243122a9
gotoAbraxas <lty7085@gmail.com>
2023-11-21 13:40:18 +0900

    fix: 형태 수정#70

--- src/component/common/CommentInformation.jsx
+++ src/component/common/CommentInformation.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { OutlinedInput, TextField } from "@mui/material";
+import { Input } from "@mui/material";
 
 const CommentInformation = ({ comment }) => {
   return (
-    <OutlinedInput
+    <Input
       id="filled-basic"
       defaultValue={comment.content}
       variant="standard"
       margin="normal"
-      InputProps={{
-        readOnly: true,
-      }}
+      readOnly
       fullWidth
     />
   );
