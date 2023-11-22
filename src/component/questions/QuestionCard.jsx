3282aae325dbd07a074677afc149e548abfb2170
gotoAbraxas <lty7085@gmail.com>
2023-11-22 10:55:23 +0900

    fix: 오타 수정 #92

--- src/component/questions/QuestionCard.jsx
+++ src/component/questions/QuestionCard.jsx
@@ -5,11 +5,17 @@ import {
   Card,
   CardContent,
   CardHeader,
-  CardMedia, Chip,
-  Container, FormControl,
+  CardMedia,
+  Chip,
+  Container,
+  FormControl,
   Grid,
-  IconButton, InputLabel, MenuItem, OutlinedInput,
-  Pagination, Select,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Pagination,
+  Select,
   TextField,
   Typography,
 } from "@mui/material";
@@ -17,11 +23,11 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { api, apiNoToken } from "../../network/api";
 import SearchCondition from "../common/SearchCondition";
-import { categorys } from "../common/Category";
+import { categorise } from "../common/Category";
 import ShowQuestionsCard from "./ShowQuestionsCard";
 import SearchIcon from "@mui/icons-material/Search";
-import {Link} from "react-router-dom";
-import {red} from "@mui/material/colors";
+import { Link } from "react-router-dom";
+import { red } from "@mui/material/colors";
 const QuestionCard = () => {
   const [data, setData] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
@@ -32,33 +38,34 @@ const QuestionCard = () => {
   const [coin, setCoin] = useState(true);
   const [searchCondition, setSearchCondition] = useState("latest");
   const [mainCategorys, setMainCategorys] = useState(
-      categorys.map((el) => {
-        return { isChecked: false, ...el };
-      })
+    categorise.map((el) => {
+      return { isChecked: false, ...el };
+    })
   );
-  const onCoinHandler = ()=>{
-    setNowPage(0)
+  const onCoinHandler = () => {
+    setNowPage(0);
     setCoin(!coin);
-  }
-  const onClickHandler = (category,idx) => {
-    if (!categoryList.includes(category)) setCategoryList([...categoryList, category]);
+  };
+  const onClickHandler = (category, idx) => {
+    if (!categoryList.includes(category))
+      setCategoryList([...categoryList, category]);
     else {
       setCategoryList(categoryList.filter((u) => u !== category));
     }
     setMainCategorys(
-        mainCategorys.map((el, index) => {
-          if (index === idx) {
-            return { ...el, isChecked: !el.isChecked };
-          } else {
-            return el;
-          }
-        })
+      mainCategorys.map((el, index) => {
+        if (index === idx) {
+          return { ...el, isChecked: !el.isChecked };
+        } else {
+          return el;
+        }
+      })
     );
-    setNowPage(0)
+    setNowPage(0);
   };
   useEffect(() => {
     getData();
-  }, [categoryList,nowPage,coin]);
+  }, [categoryList, nowPage, coin]);
   const getData = async () => {
     let link = "";
     for (let i = 0; i < categoryList.length; i++) {
@@ -66,18 +73,17 @@ const QuestionCard = () => {
     }
     link += `&orderBy=${searchCondition}`;
     if (keyword != null) {
-      link += `&keyword=${keyword}`
+      link += `&keyword=${keyword}`;
     }
     const getData = await apiNoToken(
-      `/api/v1/question/article?page=${nowPage}` +
-        link,
+      `/api/v1/question/article?page=${nowPage}` + link,
       "GET"
     );
     setData(getData.data.content);
     setTotalPage(getData.data.totalPages);
   };
   const changePage = (page) => {
-    const getPageData = page - 1
+    const getPageData = page - 1;
     setNowPage(getPageData);
   };
   const changeSearchCondition = (condition) => {
@@ -88,78 +94,78 @@ const QuestionCard = () => {
     setKeyword(getKeywordData);
   };
   return (
-      <Container sx={{ py: 8 }}>
-        <Grid
-            container
-            sx={{ justifyContent: "center", alignItems: "center" }}
-            spacing={1}
-        >
-          <Grid item md={1.5}>
-            <SearchCondition onChangeHandler={changeSearchCondition} />
-          </Grid>
-          <Grid item md={9}>
-            <OutlinedInput
-                margin="normal"
-                name="keyword"
-                placeholder="입력"
-                fullWidth
-                onBlur={onKeywordChangeHandler}
-            />
-          </Grid>
-          <Grid item md={1}>
-            <Button
-                variant="contained"
-                sx={{ height: 55 }}
-                onClick={onCoinHandler}
-                name='Search'
-            >
-              <SearchIcon />
-            </Button>
-          </Grid>
+    <Container sx={{ py: 8 }}>
+      <Grid
+        container
+        sx={{ justifyContent: "center", alignItems: "center" }}
+        spacing={1}
+      >
+        <Grid item md={1.5}>
+          <SearchCondition onChangeHandler={changeSearchCondition} />
         </Grid>
-        <Grid
-            container
-            spacing={4}
-            sx={{ justifyContent: "center", minWidth: 500 }}
-            mt={3}
-        >
-          {mainCategorys.map((category, idx) => (
-              <Grid item>
-                <IconButton
-                    sx={{ flexDirection: "column" }}
-                    onClick={() => onClickHandler(category.value,idx)}
-                >
-                  <img src={category.src} width="70" height="auto" />
-                  <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="h2"
-                      sx={{
-                        color: `${category.isChecked ? "primary.main" : "text.main"}`,
-                        fontWeight: `${category.isChecked ? "bold" : ""}`,
-                      }}
-                  >
-                    #{category.label}
-                  </Typography>
-                </IconButton>
-              </Grid>
-          ))}
-        </Grid>
-          <ShowQuestionsCard data={data} />
-        <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-        >
-          <Pagination
-              count={totalPage}
-              onChange={(event, page) => changePage(page)}
-              page={nowPage+1}
+        <Grid item md={9}>
+          <OutlinedInput
+            margin="normal"
+            name="keyword"
+            placeholder="입력"
+            fullWidth
+            onBlur={onKeywordChangeHandler}
           />
-        </Box>
-      </Container>
+        </Grid>
+        <Grid item md={1}>
+          <Button
+            variant="contained"
+            sx={{ height: 55 }}
+            onClick={onCoinHandler}
+            name="Search"
+          >
+            <SearchIcon />
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={4}
+        sx={{ justifyContent: "center", minWidth: 500 }}
+        mt={3}
+      >
+        {mainCategorys.map((category, idx) => (
+          <Grid item>
+            <IconButton
+              sx={{ flexDirection: "column" }}
+              onClick={() => onClickHandler(category.value, idx)}
+            >
+              <img src={category.src} width="70" height="auto" />
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h2"
+                sx={{
+                  color: `${category.isChecked ? "primary.main" : "text.main"}`,
+                  fontWeight: `${category.isChecked ? "bold" : ""}`,
+                }}
+              >
+                #{category.label}
+              </Typography>
+            </IconButton>
+          </Grid>
+        ))}
+      </Grid>
+      <ShowQuestionsCard data={data} />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Pagination
+          count={totalPage}
+          onChange={(event, page) => changePage(page)}
+          page={nowPage + 1}
+        />
+      </Box>
+    </Container>
   );
 };
 export default QuestionCard;
