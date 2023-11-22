00d90de8b0099c64cc5dac6b4ebb8e4577c0b8a6
gotoAbraxas <lty7085@gmail.com>
2023-11-22 10:54:57 +0900

    feat: 메인 화면 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,18 @@
+import CommonSlider from "../component/common/CommonSlider";
 import MainSlider from "../component/main/MainSlider";
-import MainCard from "../component/main/MainCard";
-
 
 const Main = () => {
-
-
-
-    return <div>
-
-        <MainSlider></MainSlider>
-        <MainCard></MainCard>
-
-
+  return (
+    <div>
+      <MainSlider></MainSlider>
+      <CommonSlider props={story} />
     </div>
+  );
+};
+export default Main;
 
-}
-export default Main
+const story = {
+  title: "성공담",
+  moreInfo: "/story", // 더보기로 가고싶은 장소
+  getData: "/api/v1/story", // 카드 데이터를 만들기 위해 데이터를 받아올 곳
+};
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -9,6 +9,7 @@ import Question from "../pages/Question";
 import MyInfo from "../pages/MyInfo";
 import QuestionPost from "../pages/QuestionPost";
 import QuestionDetails from "../pages/QuestionDetails";
+import Story from "../pages/Story";
 
 const MyRoutes = () => {
   return (
@@ -18,9 +19,11 @@ const MyRoutes = () => {
         <Route path="/signup" element={<Signup></Signup>} />
         <Route element={<Template />}>
           <Route path="/" element={<Main />} />
-          <Route path="/story/post" element={<StoryPost />} />
+
           <Route path="/info" element={<MyInfo />} />
+          <Route path="/story" element={<Story />} />
           <Route path="/story/:id" element={<StoryDetails />} />
+          <Route path="/story/post" element={<StoryPost />} />
           <Route path="/question" element={<Question />}></Route>
           <Route path="/question/:id" element={<QuestionDetails />}></Route>
           <Route path="/question/post" element={<QuestionPost />}></Route>
