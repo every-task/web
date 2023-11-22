70da23846e729875ce43384163befa23be3d1c56
gotoAbraxas <lty7085@gmail.com>
2023-11-22 11:13:11 +0900

    fix: 코드 수정 #92

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -13,6 +13,6 @@ export default Main;
 
 const story = {
   title: "성공담",
-  moreInfo: "/story", // 더보기로 가고싶은 장소
+  info: "/story", // 더보기로 가고싶은 장소
   getData: "/api/v1/story", // 카드 데이터를 만들기 위해 데이터를 받아올 곳
 };
